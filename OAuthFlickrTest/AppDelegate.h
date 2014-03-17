784ec1007269dd6ca905dd3f9733952d1c3fd89d
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2014-03-17 14:15:45 +0900

    Initial Commit

---
+++ OAuthFlickrTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OAuthFlickrTest
+//
+//  Created by Aizawa Takashi on 2014/03/17.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OAuthFlickrTestTests/OAuthFlickrTestTests.m
@@ -0,0 +1,34 @@
+//
+//  OAuthFlickrTestTests.m
+//  OAuthFlickrTestTests
+//
+//  Created by Aizawa Takashi on 2014/03/17.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OAuthFlickrTestTests : XCTestCase
+
+@end
+
+@implementation OAuthFlickrTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
